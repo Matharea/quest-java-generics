36155eb73235e1c7e8bb5da9609dbd79813c142c
Matharea <gorel.paul@gmail.com>
2019-01-22 09:44:35 +0100

    Quête streams

--- Generics.java
+++ Generics.java
@@ -21,50 +21,16 @@ public class Generics {
     List<Integer> ints = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
     List<String>  strs = Arrays.asList("tout", "titi", "ototo", "jean", "tous", "taratata");
 
-    // TODO - Ne retourner que les nombres paires.
-    ints = filter(ints, i -> i%2 == 0);
-    // TODO - Multiplier par 2 chaque élément de la liste.
-    ints = map(ints, i -> i*2);
-    for (Integer i: ints) System.out.println(i);
 
-    // TODO - Ne retourner que les Strings contenant au moins 2 lettres t.
-    strs = filter(strs, s -> s.matches(".*[t].*[t].*"));
-    // TODO - Passer en majuscule toutes les Strings
-    strs = map(strs, s -> s.toUpperCase());
-    for (String s: strs) System.out.println(s);
-  }
+      ints.stream()
+              .filter(i -> i%2 == 0)
+              .map(i -> i*2)
+              .forEach(System.out::println);
 
-  /**
-   * Returns a list consisting of the elements of a collection that match the given predicate.
-   * @param c Collection<T>: The input collection
-   * @param p The predicate
-   * @param <T> Type of the element in the collection
-   * @return A list that match the given filter.
-   */
-  private <T> List<T> filter(Collection<T> c, Predicate<T> p) {
-    List<T> result = new ArrayList<>();
-    // TODO - Ajouter le code ici
-    for (T t : c)
-      if (p.filter(t))
-        result.add(t);
-    // Retourner 'result' contenant les éléments de Collection filtrés par la méthode o.filter().
-    return result;
-  }
-
-  /**
-   * Returns a list consisting of the results of applying the given function to the elements of the collection.
-   * @param c Collection<T>: The input collection
-   * @param m The predicate
-   * @param <T> Type of the element in the collection
-   * @param <U> Type of the element in the returned list
-   * @return A list that match the given mapper.
-   */
-  private <T, U> List<U> map(Collection<T> c, Mapper<T, U> m) {
-    List<U> result = new ArrayList<>();
-    // TODO - Ajouter le code ici
-    for (T t : c)
-      result.add(m.map(t));
-    // Retourner 'result' contenant les éléments de la Collection modifiés par la méthode m.map().
-    return result;
+      strs.stream()
+              .filter(s -> s.matches(".*[t].*[t].*"))
+              .map(s -> s.toUpperCase())
+              .forEach(System.out::println);
+      
   }
 }
